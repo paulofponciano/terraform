ebdeb1755fcbfa6e6eafc7188efabca7a7a6c7cf
Paulo Ponciano <96355974+paulofponciano@users.noreply.github.com>
2022-03-27 12:18:10 -0300

    first commit

---
+++ ec2-default/terraform.tfvars
@@ -0,0 +1,3 @@
+#this file should not go to git or public access (git ignore)
+AWS_ACCESS_KEY = "YOUR_ACCESS_KEY"
+AWS_SECRET_KEY = "YOU_SECRET_KEY"
---
+++ ec2-default/vars.tf
@@ -0,0 +1,16 @@
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {
+}
+
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-1 = "ami-0c02fb55956c7d316"
+  }
+}
