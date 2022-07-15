ac409f8d8904f38e83ae7b4b9ffae3d8cd4ac1ad
Paulo Ponciano <96355974+paulofponciano@users.noreply.github.com>
2022-07-15 16:51:25 -0300

    Delete ec2-default directory

--- ec2-default/instance.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "terra1" {
-  ami           = lookup(var.AMIS, var.AWS_REGION, "") # last parameter is the default value
-  instance_type = "t2.micro"
-}
-resource "aws_instance" "terra2" {
-  ami           = lookup(var.AMIS, var.AWS_REGION, "") # last parameter is the default value
-  instance_type = "t2.micro"
-}
--- ec2-default/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-#this file should not go to git or public access (git ignore)
-AWS_ACCESS_KEY = "YOUR_ACCESS_KEY"
-AWS_SECRET_KEY = "YOUR_SECRET_KEY"
